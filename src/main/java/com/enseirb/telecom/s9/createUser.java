b7125dcf0f3790c38f09a514cb612278f786dfea
user <user@blue>
2014-11-19 17:05:33 +0100

    initial commit

---
+++ src/main/java/com/enseirb/telecom/s9/createUser.java
@@ -0,0 +1,35 @@
+package com.enseirb.telecom.s9;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+// The Java class will be hosted at the URI path "/myresource"
+@Path("/app/account/create") //need to change to /app/account/create
+public class createUser {
+
+	// TODO: update the class to suit your needs
+
+	// The Java method will process HTTP GET requests
+//	@GET
+//	// The Java method will produce content identified by the MIME Media
+//	// type "text/plain"
+//	@Produces("text/plain")
+//	public String getIt() {
+//		return "Got it!";
+//	}
+
+	@POST
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response postUser(User user) {
+
+		
+		return Response.ok().build();
+
+	}
+
+}
