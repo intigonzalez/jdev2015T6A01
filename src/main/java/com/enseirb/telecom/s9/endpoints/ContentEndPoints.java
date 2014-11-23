8623df88efea39c3a132afa814161b608dcbff06
chippekclement <rclement@enseirb-matmeca.fr>
2014-11-23 19:48:38 +0100

    Upload file Note : the location of the uploaded file (in ContentEndPoint) must be change to correspond to your OS and the location you want

--- src/main/java/com/enseirb/telecom/s9/endpoints/ContentEndPoints.java
+++ src/main/java/com/enseirb/telecom/s9/endpoints/ContentEndPoints.java
@@ -47,48 +47,48 @@ public class ContentEndPoints {
 		return Response.status(Status.SERVICE_UNAVAILABLE).build();
 	}
 	
-//
-//	@POST
-//	@Path("/upload")
-//	@Consumes(MediaType.MULTIPART_FORM_DATA)
-//	public Response uploadFile(
-//			@FormDataParam("file") InputStream uploadedInputStream,
-//			@FormDataParam("file") FormDataContentDisposition fileDetail) {
-//
-//		String uploadedFileLocation = "TODO"
-//				+ fileDetail.getFileName();
-//
-//		// save it
-//		writeToFile(uploadedInputStream, uploadedFileLocation);
-//
-//		String output = "File uploaded to : " + uploadedFileLocation;
-//
-//		return Response.status(200).entity(output).build();
-//
-//	}
-//
-//	// save uploaded file to new location
-//	private void writeToFile(InputStream uploadedInputStream,
-//			String uploadedFileLocation) {
-//
-//		try {
-//			OutputStream out = new FileOutputStream(new File(
-//					uploadedFileLocation));
-//			int read = 0;
-//			byte[] bytes = new byte[1024];
-//
-//			out = new FileOutputStream(new File(uploadedFileLocation));
-//			while ((read = uploadedInputStream.read(bytes)) != -1) {
-//				out.write(bytes, 0, read);
-//			}
-//			out.flush();
-//			out.close();
-//		} catch (IOException e) {
-//
-//			e.printStackTrace();
-//		}
-//
-//	}
+
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) {
+		// Le uploadedFileLocation doit être changé suivant le besoin
+		String uploadedFileLocation = "Desktop/"
+				+ fileDetail.getFileName();
+
+		// save it
+		writeToFile(uploadedInputStream, uploadedFileLocation);
+
+		String output = "File uploaded to : " + uploadedFileLocation;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	// save uploaded file to new location
+	private void writeToFile(InputStream uploadedInputStream,
+			String uploadedFileLocation) {
+
+		try {
+			OutputStream out = new FileOutputStream(new File(
+					uploadedFileLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			out = new FileOutputStream(new File(uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
 	
 //
 //	@PUT
