dd7e5c9fc48e984985d16994acbda441969fd7f3
oualdou <anas.oualdou@gmail.com>
2014-11-27 18:04:34 +0100

    Update account.js

--- src/main/html/account.js
+++ src/main/html/account.js
@@ -1,39 +1,42 @@
 /**
+ * Created by Anas on 27/11/2014.
+ */
+/**
  * Created by Charles-Damien on 27/11/14.
  */
-var userID = "vince@onehear.nl";
+var userID = "vince@oneear.nl";
 var app = angular.module('Account', []);
 
-    app.controller('UserController', function($http) {
-        var user = this;
-        user.person = {};
+app.controller('UserController', function($http) {
+    var user = this;
+    user.person = {};
 
-        this.getUser = function() {
-            $http.get("/api/app/account/"+userID)
-                .success(function( data, status, headers, config) {
-                    user.person = data.user;
-                })
-                .error(function( data, status, headers, config) {
-                    console.log("Failed");
-                })
-        };
-        this.putUser = function (person) {
-            var data = {};
-            data.user = person;
-            $http.put("/api/app/account/"+this.person.userID, data )
-                .success(function (data, status, headers, config)
-                {
-                    console.log("Succeed");
-                })
-                .error(function (data, status, headers, config)
-                {
-                    console.log("Failed");
-                });
-        };
-        this.getUser();
+    this.getUser = function() {
+        $http.get("/api/app/account/"+userID)
+            .success(function( data, status, headers, config) {
+                user.person = data.user;
+            })
+            .error(function( data, status, headers, config) {
+                console.log("Failed");
+            })
+    };
+    this.putUser = function (person) {
+        var data = {};
+        data.user = person;
+        $http.put("/api/app/account/"+this.person.userID, data )
+            .success(function (data, status, headers, config)
+            {
+                console.log("Succeed");
+            })
+            .error(function (data, status, headers, config)
+            {
+                console.log("Failed");
+            });
+    };
+    this.getUser();
 
 
-    });
+});
 
 app.controller('DisplayController', function() {
     this.tab = 1;
@@ -44,3 +47,27 @@ app.controller('DisplayController', function() {
         return this.tab === value;
     };
 });
+
+app.controller('listCtrl', ['$scope', function ($scope) {
+
+    $scope.friendList = {};
+
+    $scope.friendList.friend = [{
+        "name": "Steve Jobs",
+        "surname": "The Boss",
+        "email": "i-mNotHere@paradise.com"
+    },{
+        "name": "Ellie Goulding",
+        "surname": "Ms Beauty",
+        "email": "heyu@dreams.zz"
+    },{
+        "name": "Michael Stipe",
+        "surname": "Everybody",
+        "email": "lol@LAD.bib"
+    },{
+        "name": "Jeremy Clarkson",
+        "surname": "Mr car",
+        "email": "iduA@6.mir"
+    }];
+
+}]);
