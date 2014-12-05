3c0d1512e350a21f8507e8816eaf1a67243d36f7
chippekclement <rclement@enseirb-matmeca.fr>
2014-12-05 15:22:23 +0100

    just a bug fix

--- src/main/java/com/enseirb/telecom/s9/QueueConsumerApp.java
+++ src/main/java/com/enseirb/telecom/s9/QueueConsumerApp.java
@@ -12,7 +12,7 @@ public class QueueConsumerApp {
 
 	//private final static String QUEUE_NAME = "celery";
 
-	public static void test(String QUEUE_NAME) throws IOException,
+	public static void test(String queue) throws IOException,
 			InterruptedException {
 
 		ConnectionFactory factory = new ConnectionFactory();
@@ -20,15 +20,16 @@ public class QueueConsumerApp {
 		com.rabbitmq.client.Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 
-		channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+		channel.queueDeclare(queue, true, false, false, null);
 		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 		// final QueueingConsumer consumer = new QueueingConsumer(channel);
 
+		final String QUEUE_NAME = queue;
 		channel.basicConsume(QUEUE_NAME, false, new DefaultConsumer(channel) {
 			@Override
 			public void handleDelivery(String consumerTag, Envelope envelope,
 					BasicProperties properties, byte[] body) throws IOException {
-				System.out.println(body);
+				System.out.println(QUEUE_NAME);
 			}
 		});
 
