a0fc69c1e6abfde1c7714731b96797bcf0faf3db
Charles-Damien PAPOT <chadapap@gmail.com>
2014-12-18 13:42:14 +0100

    merge theme with angular-seed

---
+++ src/main/html/views/myvideos/myvideos.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.myvideos', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/myvideos', {
+    templateUrl: 'views/myvideos/myvideos.html',
+    controller: 'MyVideosCtrl'
+  });
+}])
+
+.controller('MyVideosCtrl', [function() {
+
+}]);
