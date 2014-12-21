0f11719067dfd0e310c67b1ad3560beb349a0ab8
dbourasseau <dbourasseau@enseirb-matmeca.fr>
2014-12-21 16:59:28 +0100

    Add comment

--- src/main/java/com/enseirb/telecom/s9/service/ContentService.java
+++ src/main/java/com/enseirb/telecom/s9/service/ContentService.java
@@ -4,23 +4,53 @@ import java.io.File;
 import java.io.InputStream;
 import java.util.List;
 
-import com.enseirb.telecom.s9.Authorization;
 import com.enseirb.telecom.s9.Content;
 import com.enseirb.telecom.s9.ListContent;
 
 
 public interface ContentService {
 	
+	/**
+	 * Verify if content exist on box and on server
+	 * @param contentsID to verify
+	 * @return
+	 */
 	public abstract boolean contentExist(String contentsID);
 
+	/**
+	 * get all contents from userID
+	 * @param userID of get contents
+	 * @return ListContent 
+	 */
 	public abstract ListContent getAllContentsFromUser(String userID);
 	
+	/**
+	 * get more information from a contentsID
+	 * @param contentsID to get
+	 * @return Content 
+	 */
 	public abstract Content getContent(String contentsID);
 
+	/**
+	 * send a new file (video actually) on box
+	 * @param content
+	 * @param srcfile
+	 * @param id
+	 * @return
+	 */
 	public abstract Content createContent(Content content, String srcfile, String id);
 
+	/**
+	 * update Content 
+	 * for modification groupe or action
+	 * @param content
+	 */
 	public abstract void saveContent(Content content);
 	
+	/**
+	 * delete a contents id
+	 * @param contentsID to delete
+	 */
 	public abstract void deleteContent(String contentsID);
 
 	/**
@@ -31,7 +61,17 @@ public interface ContentService {
 	public abstract void writeToFile(InputStream uploadedInputStream,
 			File dest);
 
+	/**
+	 * get all content for a list group (use from a author box)
+	 * is use to get video for a specific user
+	 * @param groupID list of group to get video
+	 * @return a list a content
+	 */
 	public abstract ListContent getAllContent(List<Integer> groupID);
 
+	/**
+	 * is use just for change status a video
+	 * @param contentsID
+	 */
 	public abstract void updateContent(String contentsID);
 }
