6f5b291039c7283eb66c1197916354a12cd66670
Chippek Clement <rclement@enseirb-matmeca.fr>
2015-01-11 14:11:58 +0100

    Interface no displayed if cookie null

--- src/main/html/home.js
+++ src/main/html/home.js
@@ -3,6 +3,25 @@
  */
 'use strict';
 
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function userAuth(){
+	if(getCookie("authentication")=="" ){
+        window.location.replace("/");	
+	}
+	
+}
+
 angular.module('myApp.index', [])
 
     .controller('IndexController', [ function () {
