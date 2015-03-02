94cf5f13cee3ede86bfe8bd70f0f9fe1cad70587
dbourasseau <dbourasseau@enseirb-matmeca.fr>
2015-03-02 16:59:41 +0100

    Change host name to 0.0.0.0

--- dvd2c-services/dvd2c-services-commons/src/main/java/com/enseirb/telecom/dngroup/dvd2c/db/DbInit.java
+++ dvd2c-services/dvd2c-services-commons/src/main/java/com/enseirb/telecom/dngroup/dvd2c/db/DbInit.java
@@ -2,6 +2,9 @@ package com.enseirb.telecom.dngroup.dvd2c.db;
 
 import java.net.UnknownHostException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.DBObject;
@@ -11,8 +14,10 @@ import com.mongodb.util.JSON;
 
 //NHE a class with only static methods should be final or abstract
 public class DbInit {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DbInit.class);
+	
 	//NHE no hard coded values allowed in the code
-	String hostname = "127.0.0.1";
+	String hostname = "0.0.0.0";
 	//NHE no hard coded values allowed in the code
 	int port = 27017;
 	String username;
@@ -21,7 +26,14 @@ public class DbInit {
 	//NHE bad method firstname https://google-styleguide.googlecode.com/svn/trunk/javaguide.html#s5.2.3-method-names
 	public static MongoClient connect() throws UnknownHostException {
 		DbInit Database = new DbInit();
-		MongoClient mongoClient = new MongoClient(Database.hostname, Database.port);
+		
+		MongoClient mongoClient;
+		try {
+			mongoClient = new MongoClient(Database.hostname, Database.port);
+		} catch (UnknownHostException e) {
+			LOGGER.error("try to connect to {hostname}:{port}");
+			throw e;
+		}
 		//NHE where is it closed ? http://api.mongodb.org/java/2.10.0/com/mongodb/Mongo.html#close%28%29
 		return mongoClient;
 	}
