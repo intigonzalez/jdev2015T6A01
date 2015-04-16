ac7d4ea7ec9eb6024f6143183481595ea4a57809
Zemelusa <zemelusa@gmail.com>
2015-04-16 14:55:06 +0200

    no more Ip adresses and Port hard coded

--- incubator/snapmail/src/main/java/com/enseirb/telecom/dngroup/snapmail/Main.java
+++ incubator/snapmail/src/main/java/com/enseirb/telecom/dngroup/snapmail/Main.java
@@ -37,6 +37,9 @@ public class Main {
 		@Option(shortName = "p", defaultValue = "9998")
 		public String getMediaHomePort();
 
+		@Option(shortName = "p_cav", defaultValue = "3310")
+		public String getClamAVPort();
+
 	}
 
 	public static void main(String[] args) throws KeyStoreException,
