27ec15001d5a02713b3f7174ccec86d63de90174
dbourasseau <dbourasseau@enseirb-matmeca.fr>
2015-04-20 19:28:33 +0200

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,40 +5,12 @@
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get -y install oracle-java8-installer maven apache2 mongodb-server git
+sudo apt-get -y install oracle-java8-installer maven mongodb-server git
 echo javax.xml.accessExternalSchema = all | sudo tee  /usr/lib/jvm/java-8-oracle/jre/lib/jaxp.properties > /dev/null
  
-# activate module on apache
-a2enmod proxy proxy_http headers
-
-
-# activate forward port 80 to 9998
-touch /etc/apache2/sites-available/mediahome.conf
-
-echo '
-<VirtualHost *:80>
-DocumentRoot /var/www/html
-
-Header always set Access-Control-Allow-Origin "*"
-Header always set Access-Control-Allow-Methods "POST, GET, OPTIONS, DELETE, PUT"
-Header always set Access-Control-Max-Age "1000"
-Header always set Access-Control-Allow-Headers "x-requested-with, Content-Type, origin, authorization, accept, client-security-token, range"
-
-ProxyPass /videos !
-ProxyPass / http://localhost:9998/
-ProxyPassReverse / http://localhost:9998/
-ProxyPreserveHost On
-</VirtualHost>
-'> /etc/apache2/sites-available/mediahome.conf
-
-a2dissite 000-default.conf 
-a2ensite mediahome.conf
-service apache2 reload
-service apache2 restart
-
 # create folder video for put videos inside 
-mkdir /var/www/html/videos
-chmod 777 /var/www/html/videos
+mkdir /var/www/html
+chown "$USER":"$USER" -R /var/www/html
 
 # compile 
 mvn clean package 
