41db08b2987005370c2a345ce86f780a22430653
dbourasseau <dbourasseau@enseirb-matmeca.fr>
2015-04-29 15:18:06 +0200

    add firends

--- dvd2c-box/src/main/html/views/friends/friends.js
+++ dvd2c-box/src/main/html/views/friends/friends.js
@@ -1,8 +1,37 @@
 'use strict';
 
-angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
+var mod = angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap','ngMockE2E'])
+mod.run(function($httpBackend) {
 
-    .config(['$routeProvider', function ($routeProvider) {
+	// returns the current list of phones
+	
+	
+	$httpBackend
+			.when('GET', /^\/api\/app\/[^//]+\/relation$/)
+			.respond(
+					function(method, url, data, headers) {
+						var data = '{"relations":{"relation":[{"actorID":"blu","firstname":"blu","surname":"blu","aprouve":1,"roleID":0},{"actorID":"blue@blue.fr","firstname":"blue","surname":"colors","aprouve":2,"roleID":0},{"actorID":"cgonzalez@viotech.net","firstname":"clement","surname":"clement","aprouve":3,"roleID":0}]}}';
+						var header = {};
+						return [ 200, data , {} ];
+
+					});
+	$httpBackend
+	.when('PUT', /^\/api\/app\/[^//]+\/relation\/[^//]+$/)
+	.respond(
+			function(method, url, data, headers) {
+				var data = '';
+				var header = {};
+				return [ 200, data , {} ];
+
+			});
+	
+	
+	$httpBackend.whenGET(/views/).passThrough();
+	
+	
+});
+
+    mod.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/friends', {
             templateUrl: 'views/friends/friends.html',
             controller: 'FriendsCtrl'
@@ -11,7 +40,7 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
     .controller('FriendsCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
 
         var videos = this;
-        //Determine the right steraming protocol.
+        // Determine the right steraming protocol.
         var userAgent = $window.navigator.userAgent;
         if ( userAgent.indexOf("Chrome") >= 0 || userAgent.indexOf("Windows") >=0 || userAgent.indexOf("Chromium") >=0 ) {
             videos.prefix = 'dash';
@@ -22,12 +51,14 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
             videos.suffix = 'hls/playlist.m3u8';
         }
 
-        //Init Objects
+        // Init Objects
         $scope.FriendsCtrl ={}; // Just use for tabs
 
-        $scope.FriendProfileController = {}; // use to give all the items to the other controller
+        $scope.FriendProfileController = {}; // use to give all the items to
+												// the other controller
         $scope.FriendProfileController.getVideoContent = function(friend) {
-            $scope.FriendProfileController.videos = []; // object to store videos
+            $scope.FriendProfileController.videos = []; // object to store
+														// videos
             $http.get(PREFIX_RQ+"/api/app/"+userID+"/relation/"+friend.actorID +"/content")
                 .success(function(data, status, headers, config) {
                     if ( data.contents !== "" ) {
@@ -37,7 +68,7 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
                         else {
                             $scope.FriendProfileController.videos = data.contents.content;
                         }
-                        //console.log($scope.FriendProfileController.videos);
+                        // console.log($scope.FriendProfileController.videos);
                     }
                 })
                 .error(function (data, status, headers, config){
@@ -75,7 +106,7 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
             $scope.FriendsCtrl.tabValue = int;
         }
         $scope.DisplayFriendProfile = function(friend) {
-            //console.log("Display Friend Profile");
+            // console.log("Display Friend Profile");
             $scope.FriendsCtrl.SetTab(1);
             $scope.FriendProfileController.friend= friend;
             $scope.FriendProfileController.getVideoContent(friend);
@@ -91,16 +122,17 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
     .controller('FriendsListController', ['$scope', '$http', '$modal', '$log', function ($scope, $http, $modal, $log) {
 
         var friends = this; // This element to get it easily
-        friends.list = []; //Friend list into the controller
-        friends.addFriendSuccess =null; //boolean to change the color of add Friend button
+        friends.list = []; // Friend list into the controller
+        friends.addFriendSuccess =null; // boolean to change the color of add
+										// Friend button
         friends.listRoles = [
             {"roleID":"0" , "roleName":"public", "info":"Seen by all your relations"},
             {"roleID":"1" , "roleName":"Family", "info":"Seen by all your family only"},
             {"roleID":"2" , "roleName":"Friends", "info":"Seen by all your friends only"},
             {"roleID":"3" , "roleName":"Pro", "info":"Seen by all your professional contacts"},
-        ];  //List of roles
+        ];  // List of roles
 
-        // *****************  Get RoleList****************
+        // ***************** Get RoleList****************
         this.getRoleList = function() {
             $http.get(PREFIX_RQ+"/api/app/"+userID+"/role")
                 .success(function(data, status, headers, config) {
@@ -117,9 +149,11 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
                     console.log("Failed getting Roles list");
                 })
         };
-        //this.getRoleList(); //Disabled because the endpoint doesn't exist yet !
+        // this.getRoleList(); //Disabled because the endpoint doesn't exist yet
+		// !
 
-        //This function is called to display the list of roles a relation belongs to.
+        // This function is called to display the list of roles a relation
+		// belongs to.
         this.generateRoleList = function(friend) {
             var result = [];
             if (friend.roleID !== undefined) {
@@ -136,7 +170,7 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
             return result;
         }
 
-        // *****************  Get FriendList ****************
+        // ***************** Get FriendList ****************
         
         this.getFriendList = function() {
             $http.get(PREFIX_RQ+"/api/app/"+userID+"/relation")
@@ -149,8 +183,11 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
                             friends.list = data.relations.relation;
                         }
                     }
-                    //console.log(friends.list);
-                    friends.addFriendSuccess =null; //reset the value if the function is called from AddFriend Callback Success
+                    // console.log(friends.list);
+                    friends.addFriendSuccess =null; // reset the value if the
+													// function is called from
+													// AddFriend Callback
+													// Success
                 })
                 .error(function (data, status, headers, config){
                     console.log("Failed getting Friend list");
@@ -158,7 +195,7 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
         };
         this.getFriendList();
 
-        //Get Aprouve Status to adapt display
+        // Get Aprouve Status to adapt display
         this.isStatus = function(friend, str) {
             if (friend.aprouve == str) {
                 return true;
@@ -180,13 +217,13 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
                 })
         }
 
-        //Function to call to accept a relation request
+        // Function to call to accept a relation request
         this.AprouveRelation = function(friend) {
             friend.aprouve = 3;
             friends.updateRelation(friend);
         }
 
-        //function to change the color of add Friend button
+        // function to change the color of add Friend button
         this.isFriendAddingSuccess = function() {
             if (friends.addFriendSuccess == null) {
                 return "";
@@ -200,7 +237,7 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
             }
         }
 
-        // ********  Add a Friend to the friendList **********
+        // ******** Add a Friend to the friendList **********
         $scope.addRelation = function(friend_userID) {
 
             $http.post(PREFIX_RQ + "/api/app/" + userID + "/relation/"+friend_userID)
@@ -239,7 +276,7 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
             $scope.open(friend);
         }
 
-        //Enable Modal to edit roles
+        // Enable Modal to edit roles
         $scope.open = function (friend, size) {
             var modalInstance = $modal.open({
                 templateUrl: 'myModalContent.html',
@@ -263,23 +300,23 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
         };
 
         // ***** Collapse *****
-        //friends.collapse = null;
-        //friends.isCollapsed = function(friend) {
-        //    if (friends.collapse == friend.actorID ) {
-        //        return false;
-        //    }
-        //    else {
-        //        return true;
-        //    }
-        //}
-        //friends.setCollapsed = function(friend) {
-        //    if (friends.isCollapsed(friend) == false) {
-        //        friends.collapse = null;
-        //    }
-        //    else {
-        //        friends.collapse = friend.actorID;
-        //    }
-        //}
+        // friends.collapse = null;
+        // friends.isCollapsed = function(friend) {
+        // if (friends.collapse == friend.actorID ) {
+        // return false;
+        // }
+        // else {
+        // return true;
+        // }
+        // }
+        // friends.setCollapsed = function(friend) {
+        // if (friends.isCollapsed(friend) == false) {
+        // friends.collapse = null;
+        // }
+        // else {
+        // friends.collapse = friend.actorID;
+        // }
+        // }
 
 
     }])
@@ -288,7 +325,7 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
     .controller('ModalInstanceCtrl', ['$scope', '$modalInstance', 'listRoles', 'friend', function ($scope, $modalInstance, listRoles, friend) {
 
 
-        //console.log(friend);
+        // console.log(friend);
         $scope.listRoles = angular.copy(listRoles);
         if (friend.roleID === undefined) {
         } else {
@@ -303,10 +340,10 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
                 $scope.listRoles[index].value=true;
             }
         }
-        //console.log(listRoles);
+        // console.log(listRoles);
 
         $scope.ok = function () {
-            //console.log($scope.listRoles);
+            // console.log($scope.listRoles);
             friend.roleID = [];
             angular.forEach($scope.listRoles, function(role) {
                 if (role.value == true) {
@@ -314,7 +351,7 @@ angular.module('myApp.friends', ['ngRoute', 'ui.bootstrap'])
                 }
             });
 
-            //console.log(friend);
+            // console.log(friend);
             $modalInstance.close(friend);
         };
 
