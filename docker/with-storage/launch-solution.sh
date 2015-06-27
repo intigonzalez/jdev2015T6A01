ba0d4a80555a020792fb39774c9d752cf75fa375
Nicolas Herbaut <nherbaut@labri.fr>
2015-06-27 19:10:33 +0200

    working with docker compose

---
+++ docker/with-storage/launch-solution.sh
@@ -0,0 +1,11 @@
+sudo docker stop broker frontend worker1 box storage
+sudo docker rm broker frontend worker1 box storage
+
+sudo docker run -d --name broker -P rabbitmq:3.5.3
+sleep 3
+sudo docker run -d --name worker1 --link broker:amqp nherbaut/worker
+sudo docker run -d --name box -P --link broker:amqp nherbaut/dvd2c-box
+sudo docker run -d --name frontend -p 8080:8080 --link box:box nherbaut/frontend 
+sudo docker run -d --name storage -P --link box:box nherbaut/dummy-storage
+
+
