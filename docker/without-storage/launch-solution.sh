fc6f509879833ecbd6179e85c9cf908527a154e2
Nicolas Herbaut <nherbaut@labri.fr>
2015-06-28 21:09:10 +0200

    no need to wait!

--- docker/without-storage/launch-solution.sh
+++ docker/without-storage/launch-solution.sh
@@ -2,7 +2,6 @@ sudo docker stop broker frontend worker1 box storage
 sudo docker rm broker frontend worker1 box storage
 
 sudo docker run -d --name broker -P rabbitmq:3.5.3
-sleep 3
 sudo docker run -d --name worker1 --link broker:amqp nherbaut/worker
 sudo docker run -d --name box -P --link broker:amqp nherbaut/dvd2c-box
 sudo docker run -d --name frontend -p 8080:8080 --link box:box nherbaut/frontend 
